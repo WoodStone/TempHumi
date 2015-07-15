0291c19c556c64fe826f93eb40522f48c285045d
WoodStone <golfflames@gmail.com>
2015-07-15 23:22:47 +0200

    More stuff

--- src/main/java/no/vestein/raspberry/temphumi/Main.java
+++ src/main/java/no/vestein/raspberry/temphumi/Main.java
@@ -1,6 +1,5 @@
 package no.vestein.raspberry.temphumi;
 
-
 import com.googlecode.charts4j.Color;
 
 import java.util.Arrays;
@@ -8,32 +7,35 @@ import java.util.LinkedList;
 
 public class Main {
 
+    private static boolean debug = true;
+
     public static void main(String[] args) {
-//        Sensor sensor = new Sensor(11, 4);
-//        SensorData data = new SensorData(Constants.NUM_OF_READINGS);
-//
-//        sensor.addListener(data);
+        if (debug) {
+            debug();
+        } else {
+            pi();
+        }
+    }
 
-//        while (true) {
-//            sensor.update();
-//
-//            System.out.println(ChartGen.makeChart(data.getData()));
-//            try {
-//                Thread.sleep(Constants.SLEEP_TIME);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        for (int i = 0; i < 13; i++) {
-//            sensor.update();
-//            try {
-//                Thread.sleep(Constants.SLEEP_TIME);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        System.out.println(ChartGen.makeChart(data.getData()));
+    public static void pi() {
+        Sensor sensor = new Sensor(11, 4);
+        SensorData data = new SensorData(Constants.NUM_OF_READINGS);
 
+        sensor.addListener(data);
+
+        while (true) {
+            sensor.update();
+
+            System.out.println(data.getTempChart());
+            try {
+                Thread.sleep(Constants.SLEEP_TIME);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void debug() {
         LinkedList<Double> temp = new LinkedList<>(Arrays.asList(20.0, 23.0, 25.0, 26.0, 22.0, 22.0, 25.0, 25.0, 21.0, 15.0, 22.0));
         LinkedList<Double> humi = new LinkedList<>(Arrays.asList(40.0, 50.0, 44.0, 54.0, 53.0, 50.0, 45.0, 43.0, 40.0, 35.0, 40.0));
 
