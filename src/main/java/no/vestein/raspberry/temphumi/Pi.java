127f3b693dfaf43510a36353982c9caed2f9f9d7
WoodStone <golfflames@gmail.com>
2015-07-19 23:51:08 +0200

    Adds todo for threading data mining

---
+++ src/main/java/no/vestein/raspberry/temphumi/Pi.java
@@ -0,0 +1,10 @@
+package no.vestein.raspberry.temphumi;
+
+public class Pi implements Runnable {
+
+    @Override
+    public void run() {
+        //TODO implement thread for data mining
+    }
+
+}
