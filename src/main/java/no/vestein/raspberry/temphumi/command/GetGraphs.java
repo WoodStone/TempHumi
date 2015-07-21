1817be4d3150fd013a6b909399dc3b84681cbb30
WoodStone <golfflames@gmail.com>
2015-07-22 01:12:59 +0200

    Adds the getGraph command

--- src/main/java/no/vestein/raspberry/temphumi/command/GetGraphs.java
+++ src/main/java/no/vestein/raspberry/temphumi/command/GetGraphs.java
@@ -1,11 +1,16 @@
 package no.vestein.raspberry.temphumi.command;
 
+import no.vestein.raspberry.temphumi.sensor.SensorTask;
+
 import java.util.List;
 
 public class GetGraphs implements ICommand {
 
     public void processCommand(List<String> strings) {
-
+        System.out.println("-------------------------");
+        System.out.println(SensorTask.data.getTempChart());
+        System.out.println(SensorTask.data.getHumiChart());
+        System.out.println("-------------------------");
     }
 
 }
