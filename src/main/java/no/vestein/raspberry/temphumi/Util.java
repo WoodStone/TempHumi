76edd331ed16eb79e7575df9dddd724e83095f47
WoodStone <golfflames@gmail.com>
2015-07-24 21:13:52 +0200

    Adds utility class

---
+++ src/main/java/no/vestein/raspberry/temphumi/Util.java
@@ -0,0 +1,18 @@
+package no.vestein.raspberry.temphumi;
+
+import java.util.Random;
+
+public class Util {
+    private Util() {}
+
+    private static Random rand = new Random();
+
+    public static int randInt(int min, int max) {
+        return rand.nextInt(max - min + 1) + min;
+    }
+
+    public static int randInt(int max) {
+        return randInt(0, max);
+    }
+
+}
