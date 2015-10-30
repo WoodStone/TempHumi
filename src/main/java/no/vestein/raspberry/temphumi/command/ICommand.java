e50505b64f47426f78c560111eda45ccba97183a
WoodStone <vesteindahl@hotmail.com>
2015-10-30 18:06:06 +0100

    Adds comment to ICommand.java

--- src/main/java/no/vestein/raspberry/temphumi/command/ICommand.java
+++ src/main/java/no/vestein/raspberry/temphumi/command/ICommand.java
@@ -2,6 +2,9 @@ package no.vestein.raspberry.temphumi.command;
 
 import java.util.List;
 
+/**
+ * Represents a command
+ */
 public interface ICommand {
 
     void processCommand(List<String> strings);
